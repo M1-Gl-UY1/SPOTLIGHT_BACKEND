150ffc821637b06c18e93dee675b029c9837d79d
emilekmdDev <emilekamdem.c@gmail.com>
2025-12-10 13:03:16 +0100

    offer and prestation registry on eureka

--- offerandprestation/src/main/java/com/spotlight/offerandprestation/OfferandprestationApplication.java
+++ offerandprestation/src/main/java/com/spotlight/offerandprestation/OfferandprestationApplication.java
@@ -2,8 +2,10 @@ package com.spotlight.offerandprestation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OfferandprestationApplication {
 
 	public static void main(String[] args) {
