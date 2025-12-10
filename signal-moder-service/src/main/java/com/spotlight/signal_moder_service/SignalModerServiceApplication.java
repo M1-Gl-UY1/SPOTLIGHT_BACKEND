2b899d8489c9b8c9e4196690d0bb3e9d630ae0af
imelda2627 <imeldaktis@gmail.com>
2025-12-10 13:13:46 +0100

    modification du main de signal et moder

--- signal-moder-service/src/main/java/com/spotlight/signal_moder_service/SignalModerServiceApplication.java
+++ signal-moder-service/src/main/java/com/spotlight/signal_moder_service/SignalModerServiceApplication.java
@@ -2,9 +2,9 @@ package com.spotlight.signal_moder_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SignalModerServiceApplication {
 
