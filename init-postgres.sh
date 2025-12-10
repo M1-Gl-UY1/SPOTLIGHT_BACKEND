c9041daebbaf925a2a9e73b5548295cc35f308cd
emilekmdDev <emilekamdem.c@gmail.com>
2025-12-10 16:43:47 +0100

    update docker file

---
+++ init-postgres.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE user_db;
+    CREATE DATABASE signal_db;
+    GRANT ALL PRIVILEGES ON DATABASE user_db TO "$POSTGRES_USER";
+    GRANT ALL PRIVILEGES ON DATABASE signal_db TO "$POSTGRES_USER";
+EOSQL
