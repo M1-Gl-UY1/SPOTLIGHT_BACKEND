ab86343f1fe88deec41ba7b295628bda8ce848b3
idriss Tagny <idrisstagny@gmail.com>
2025-12-12 14:45:28 +0100

    feat: ajout de nouveaux services et mise à jour des configurations Docker et Kubernetes

--- spotlight_catalogue_presentation/serviceCataloguePrestations/models.py
+++ spotlight_catalogue_presentation/serviceCataloguePrestations/models.py
@@ -8,8 +8,8 @@ class Categorie(models.Model):
     nom = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
     parent = models.ForeignKey(
-        'self', 
-        on_delete=models.CASCADE, 
+        'self',
+        on_delete=models.CASCADE,
         null=True, 
         blank=True,
         related_name='sous_categories'
